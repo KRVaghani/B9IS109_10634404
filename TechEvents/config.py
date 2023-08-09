1b4cd7b7e312910b674090f4cee7a0f3925300af
Kaushik <kaushik@Kaushiks-MacBook-Pro.local>
2023-08-09 07:29:24 +0100

    add code

---
+++ TechEvents/config.py
@@ -0,0 +1,16 @@
+SECRET_KEY = 'djiek1o39203912'
+DEBUG = True
+
+# MONGODB_DB = 'techevents'
+# MONGODB_HOST = 'cluster0.lz3rp7q.mongodb.net'
+# # MONGODB_PORT = 27017
+# MONGODB_USERNAME = 'krvaghani111'
+# MONGODB_PASSWORD = 'P3lQOYhmsgZtrY5W'
+
+MONGODB_SETTINGS = {
+    'host': 'mongodb://krvaghani111:P3lQOYhmsgZtrY5W@ac-zasdxib-shard-00-00.lz3rp7q.mongodb.net:27017,ac-zasdxib-shard-00-01.lz3rp7q.mongodb.net:27017,ac-zasdxib-shard-00-02.lz3rp7q.mongodb.net:27017/techevents?ssl=true&replicaSet=atlas-prdefb-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
+
+PROJECT_ID = '64d2db9d57db174a382b0731'
+CLOUD_STORAGE_BUCKET = 'STORAGE_BUCKET'
+
---
+++ TechEvents/run.py
@@ -0,0 +1,4 @@
+from application import create_app
+app = create_app(config='config')
+
+app.run(host='0.0.0.0', port=8080)
