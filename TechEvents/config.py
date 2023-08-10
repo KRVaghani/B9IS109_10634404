5791e572b4f63db0e57021aeb0facf41af5420ff
Kaushik <kaushik@Kaushiks-MacBook-Pro.local>
2023-08-11 00:42:01 +0100

    changes add

--- TechEvents/config.py
+++ TechEvents/config.py
@@ -10,7 +10,6 @@ DEBUG = True
 MONGODB_SETTINGS = {
     'host': 'mongodb://krvaghani111:P3lQOYhmsgZtrY5W@ac-zasdxib-shard-00-00.lz3rp7q.mongodb.net:27017,ac-zasdxib-shard-00-01.lz3rp7q.mongodb.net:27017,ac-zasdxib-shard-00-02.lz3rp7q.mongodb.net:27017/techevents?ssl=true&replicaSet=atlas-prdefb-shard-0&authSource=admin&retryWrites=true&w=majority'
 }
-
-PROJECT_ID = '64d2db9d57db174a382b0731'
-CLOUD_STORAGE_BUCKET = 'STORAGE_BUCKET'
+PROJECT_ID = 'techevents-395404'
+CLOUD_STORAGE_BUCKET = 'techeventkrvaghani'
 
